54b0adb4f396e542b2a4bf7c703a9fe1ccc9527e
caj67 <54556483+caj67@users.noreply.github.com>
2022-06-26 15:23:42 -0400

    create exercise widget, add gif support

--- lib/src/common_widgets/font_styles.dart
+++ lib/src/common_widgets/font_styles.dart
@@ -4,4 +4,10 @@ class MyTextStyleBase {
   static const big = TextStyle(fontSize: 22);
   static const medium = TextStyle(fontSize: 18);
   static const small = TextStyle(fontSize: 14);
+  static const bigWhite =
+      TextStyle(fontSize: 22, color: Color.fromARGB(255, 255, 255, 255));
+  static const mediumWhite =
+      TextStyle(fontSize: 18, color: Color.fromARGB(255, 255, 255, 255));
+  static const smallWhite =
+      TextStyle(fontSize: 14, color: Color.fromARGB(255, 255, 255, 255));
 }
