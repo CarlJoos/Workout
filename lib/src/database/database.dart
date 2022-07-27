e812d8af6e7be766f99d23c1b84df4cb3b05f710
cbrandt6 <cbrandt6@ycp.edu>
2022-07-26 22:04:46 -0400

    More changing to match docs

--- lib/src/database/database.dart
+++ lib/src/database/database.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
 import 'package:drift_sqflite/drift_sqflite.dart';
+import "package:path_provider/path_provider.dart";
+import "package:path/path.dart" as p;
 
 import 'exercises.dart';
 part 'database.g.dart';
@@ -11,13 +16,17 @@ part 'database.g.dart';
 class AppDatabase extends _$AppDatabase {
   AppDatabase()
       // Specify the location of the database file
-      : super((SqfliteQueryExecutor.inDatabaseFolder(
-          path: 'db.sqlite',
-          // Good for debugging - prints SQL in the console
-          logStatements: true,
-        )));
+      : super(_openConnection());
 
   // Bump this when changing tables and columns.
   @override
   int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
 }
